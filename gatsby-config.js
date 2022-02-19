69fe6b5869d7733f5c2119da8b2b452d2140fb40
h-gj <690631890@qq.com>
2022-02-19 19:26:19 +0800

    feat: finish system

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-      title: `Gatsby`,
+      title: `Gorgine's Blog`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
@@ -14,5 +14,6 @@ module.exports = {
         }
       },
       "gatsby-plugin-mdx",  
+      "gatsby-transformer-sharp",
     ],
 }
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,16 +2,18 @@ import * as React from 'react'
 import { graphql, Link } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
+import {cArticle, cArticleSubtitle, cArticleTitle} from './blog.module.css'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {
         data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          <article className={cArticle} key={node.id}>
+            <Link className={cArticleTitle} to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+
+            {/* <h2>{node.frontmatter.title}</h2> */}
+            <p className={cArticleSubtitle}>Posted: {node.frontmatter.date}</p>
           </article>
         ))
       }
